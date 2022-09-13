645cb17275a937bc83c961668016c46a9c8e0737
xdMorganxd <adik.pro94@gamil.com>
2022-09-13 08:42:59 +0600

    button

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,62 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter Container Example"),
-        ),
-        body: Container(
-          padding: EdgeInsets.all(30),
-          margin: EdgeInsets.all(15),
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.black, width: 4),
-            borderRadius: BorderRadius.circular(25),
-            boxShadow: [
-              new BoxShadow(
-                color: Colors.blueAccent,
-                offset: new Offset(6, 6),
-              ),
-            ],
-          ),
-          child: Text(
-              "Привет Мир!",
-              style: TextStyle(fontSize: 25,color: Colors.orange)),
+        appBar: AppBar(centerTitle: true,
+          title: Text("Hello World"),),
+        body: Center(
+            child: Column(
+              children: [
+                SizedBox(height: 100,),
+
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shadowColor: Colors.grey,
+                        primary: Colors.grey,
+                        fixedSize: Size(100, 70),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)
+                        )
+                    ),
+                    onPressed: (){}, child:Text("кнопка") ),
+                SizedBox(height: 70,),
+
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shadowColor: Colors.grey,
+                        primary: Colors.grey,
+                        fixedSize: Size(120, 70),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)
+                        )
+                    ),
+                    onPressed: (){}, child:Text("кнопка") ),
+                SizedBox(height: 70,),
+
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shadowColor: Colors.grey,
+                        primary: Colors.grey,
+                        fixedSize: Size(150, 70),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)
+                        )
+                    ),
+                    onPressed: (){}, child:Text("кнопка") ),
+
+              ],
+            )
         ),
       ),
     );
