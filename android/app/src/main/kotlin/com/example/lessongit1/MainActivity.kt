51bb8c221f6478b43016973562137ce81516389a
xdMorganxd <adik.pro94@gmail.com>
2022-10-17 21:39:16 +0600

    commit

---
+++ android/app/src/main/kotlin/com/example/lessongit1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lessongit1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/model/user.dart
@@ -0,0 +1,9 @@
+class User {
+  late String id;
+  late String name;
+  late String phone;
+  late String email;
+  late String country;
+  late String gender;
+  late String story;
+}
